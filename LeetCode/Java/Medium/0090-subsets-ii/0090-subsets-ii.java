721f8cb1eb0ca082fcd8f973c9ea45d0ed2970e7
jejes323 <jejes323@gmail.com>
2026-05-02 16:00:18 +0900

    Time: 3 ms (56.25%), Space: 45.4 MB (10.44%) - LeetHub

---
+++ LeetCode/Java/Medium/0090-subsets-ii/0090-subsets-ii.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        boolean[] visited = new boolean[nums.length];
+        Arrays.sort(nums);
+        return dfs(nums, 0, new ArrayList<>(), visited);
+    }
+    private List<List<Integer>> dfs(int[] nums, int start, List<Integer> curr, boolean[] visited) {
+        List<List<Integer>> answer = new ArrayList<>();
+        answer.add(new ArrayList<>(curr));
+        for(int i = start; i < nums.length; i++) {
+            if(i > 0 && nums[i] == nums[i-1] && !visited[i-1]) continue;
+            visited[i] = true;
+            curr.add(nums[i]);
+            answer.addAll(dfs(nums, i+1, curr, visited));
+            visited[i] = false;
+            curr.remove(curr.size() - 1);
+        }
+        return answer;
+    }
+}
